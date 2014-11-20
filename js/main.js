4e9fc62b14bbbab1b7df39cc37f8ab220758bf8b
ITG_MonsterGeek <alexlegrem@gmail.com>
2014-11-20 15:51:42 +0100

    Oops !
    Damn it ! i forgot some files...
    
    Add a XML sample for demonstration purpose.
    
    Now all is good :)

---
+++ js/main.js
@@ -0,0 +1,14 @@
+﻿(function ($) {
+	// DOM Ready
+	$(function () {
+
+        $('#button_display_xml').bind('click', function () {
+            var callable = $('#button_display_xml').val();
+            $.post('afficher_xml.php',{get_xml : callable}, function(resultat){
+                $('#bloc_affichage_xml').html(resultat);
+                $('#bloc_affichage_xml').show();
+            })
+        });
+
+	});
+})(jQuery);
